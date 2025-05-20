ce89c16e936d80a1f9b310ea9f55aa09cba521ab
david <david.levin.1986@gmail.com>
2025-05-20 11:42:35 +0300

    Copy Reports

--- Remote_Monitor/performance_checker.sh
+++ Remote_Monitor/performance_checker.sh
@@ -9,28 +9,28 @@
   
 # Usage:
 # Jast run the script 
-echo "" >> /tmp/$(hostname)performance.log
-date >> /tmp/$(hostname)performance.log
-echo "-------------------------------------------------------" >> /tmp/$(hostname)performance.log
-echo "Testing Internet Connection... " >> /tmp/$(hostname)performance.log
+echo "" >> /tmp/$(hostname)_performance.log
+date >> /tmp/$(hostname)_performance.log
+echo "-------------------------------------------------------" >> /tmp/$(hostname)_performance.log
+echo "Testing Internet Connection... " >> /tmp/$(hostname)_performance.log
 ping -c 1 google.com &> /dev/null
 if [ "$?" -eq 0 ] ; then
-echo "Internet Connected Sucssesfuly :" >> /tmp/$(hostname)performance.log
+echo "Internet Connected Sucssesfuly :" >> /tmp/$(hostname)_performance.log
 else
-echo "Internet Is Disconnected :" >> /tmp/$(hostname)performance.log
+echo "Internet Is Disconnected :" >> /tmp/$(hostname)_performance.log
 
 fi 
- echo "-------------------------------------------------------" >> /tmp/$(hostname)performance.log
-  echo "" >> /tmp/$(hostname)performance.log
+ echo "-------------------------------------------------------" >> /tmp/$(hostname)_performance.log
+  echo "" >> /tmp/$(hostname)_performance.log
 
-echo "RAM Usage :" >> /tmp/$(hostname)performance.log
-free -h | grep -i "Mem" >> /tmp/$(hostname)performance.log
-echo "-------------------------------------------------------" >> /tmp/$(hostname)performance.log
-echo "" >> /tmp/$(hostname)performance.log
-echo "Swap Usage :" >> /tmp/$(hostname)performance.log
-free -h | grep -i "Swap" >> /tmp/$(hostname)performance.log
-echo "-------------------------------------------------------" >> /tmp/$(hostname)performance.log
-echo "" >> /tmp/$(hostname)performance.log
-echo "Disk Usage :" >> /tmp/$(hostname)performance.log
-df -h >> /tmp/$(hostname)performance.log
-echo "" >> /tmp/$(hostname)performance.log
+echo "RAM Usage :" >> /tmp/$(hostname)_performance.log
+free -h | grep -i "Mem" >> /tmp/$(hostname)_performance.log
+echo "-------------------------------------------------------" >> /tmp/$(hostname)_performance.log
+echo "" >> /tmp/$(hostname)_performance.log
+echo "Swap Usage :" >> /tmp/$(hostname)_performance.log
+free -h | grep -i "Swap" >> /tmp/$(hostname)_performance.log
+echo "-------------------------------------------------------" >> /tmp/$(hostname)_performance.log
+echo "" >> /tmp/$(hostname)_performance.log
+echo "Disk Usage :" >> /tmp/$(hostname)_performance.log
+df -h >> /tmp/$(hostname)_performance.log
+echo "" >> /tmp/$(hostname)_performance.log
